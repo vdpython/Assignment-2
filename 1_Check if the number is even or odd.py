a08e701fad23543ea8276b755ba3d80525726974
VP1323 <vlovep1323@gmail.com>
2025-04-28 02:19:03 +0530

    Assignment 2

---
+++ 1_Check if the number is even or odd.py
@@ -0,0 +1,7 @@
+# Program to check if a number is even or odd
+
+input_number = int(input("Enter a number:"))
+if input_number % 2 ==0:
+  print(input_number , "is an even number")
+else:
+  print(input_number , "is an odd number")
---
+++ 2_Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,7 @@
+# Program to calculate the sum of 1 to 50 numbers using for Loop
+
+sum = 0
+for i in range(1, 51):
+  sum = sum + i
+
+print("The sum of numbers from 1 to 50 is: ",sum)
